e1d53bdf6e1b83179303fd0903252412cb36e8ef
Fernando Gonçalves <fernando_jcg22@hotmail.com>
2024-08-20 08:55:11 +0000

    Make migrations  data, and creat a views

--- portfolio_project/portfolio/views.py
+++ portfolio_project/portfolio/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import SchoolProject, HackathonProject
+
+def home_view(request):
+    return render(request, 'home.html')
+
+def about_view(request):
+    return render(request, 'about.html')
+
+def cv_view(request):
+    return render(request, 'cv.html')
+
+def contact_view(request):
+    return render(request, 'contact.html')
+
+def school_projects_view(request):
+    projects = SchoolProject.objects.all()
+    return render(request, 'school_projects.html', {'projects': projects})
+
+def hackathon_projects_view(request):
+    projects = HackathonProject.objects.all()
+    return render(request, 'hackathon_projects.html', {'projects': projects})
