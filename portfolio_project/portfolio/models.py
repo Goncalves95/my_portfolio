5a6f0b4fb4a93e462e21f3079fd61037d19f53e0
Fernando Gonçalves <fernando_jcg22@hotmail.com>
2024-08-20 08:53:59 +0000

    Model.py - Starting model school project and hackathon projets

--- portfolio_project/portfolio/models.py
+++ portfolio_project/portfolio/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class SchoolProject(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='school_projects/')
+
+class HackathonProject(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='hackathon_projects/')
